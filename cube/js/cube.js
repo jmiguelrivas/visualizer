962a0d7706c0600c9adf1b781f75f7737f11f19b
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-12-02 12:14:03 -0800

    refactoring the cube

--- cube/js/cube.js
+++ cube/js/cube.js
@@ -4,226 +4,183 @@ import { OrbitControls } from "../modules/OrbitControls.js";
 
 const rad = Math.PI / 180;
 
-const data = {
-  attrs: [],
-  winHeight: undefined,
-  winWidth: undefined,
-  camera: undefined,
-  scene: undefined,
-  renderer: undefined,
-  colors: Object.values(gColors),
-  linesGroup: undefined,
-  gridHelper: undefined,
-  gridToggle: false,
-  guidesToggle: false,
-  active: false,
-
-  minPolarAngle: 0,
-  maxPolarAngle: 120 * rad,
-  minDistance: 120,
-  maxDistance: 500,
-
-  controls: undefined,
-};
-
 customElements.define(
   "cube-visualizer",
   class extends HTMLElement {
+    #data = {
+      attrs: [],
+      winHeight: undefined,
+      winWidth: undefined,
+      camera: undefined,
+      scene: undefined,
+      renderer: undefined,
+      colors: Object.values(gColors),
+      linesGroup: undefined,
+      gridHelper: undefined,
+      gridToggle: false,
+      guidesToggle: false,
+      active: false,
+
+      minPolarAngle: 0,
+      maxPolarAngle: 120 * rad,
+      minDistance: 120,
+      maxDistance: 500,
+
+      controls: undefined,
+    };
+
     constructor() {
       super();
-    }
-
-    createLine(color, origin, destination) {
-      const lineMaterial = new THREE.LineBasicMaterial({
-        color: `rgb(${color})`,
-      });
-      const points = [];
-      points.push(new THREE.Vector3(origin[0], origin[1], origin[2]));
-      points.push(
-        new THREE.Vector3(destination[0], destination[1], destination[2])
-      );
-      const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-      const line = new THREE.Line(lineGeometry, lineMaterial);
-      return line;
-    }
-
-    createGuides() {
-      const red = this.createLine(
-        "255,0,0",
-        [-255 / 2, 0, -255 / 2],
-        [255 / 2, 0, -255 / 2]
-      );
-      const blue = this.createLine(
-        "0,0,255",
-        [-255 / 2, 0, -255 / 2],
-        [-255 / 2, 0, 255 / 2]
-      );
-      const green = this.createLine(
-        "0,255,0",
-        [-255 / 2, 0, -255 / 2],
-        [-255 / 2, 255, -255 / 2]
-      );
-      const cyan = this.createLine(
-        "0,255,255",
-        [-255 / 2, 0, -255 / 2],
-        [-255 / 2, 255, 255 / 2]
-      );
-      const magenta = this.createLine(
-        "255,0,255",
-        [-255 / 2, 0, -255 / 2],
-        [255 / 2, 0, 255 / 2]
-      );
-      const yellow = this.createLine(
-        "255,255,0",
-        [-255 / 2, 0, -255 / 2],
-        [255 / 2, 255, -255 / 2]
-      );
-      const white = this.createLine(
-        "255,255,255",
-        [-255 / 2, 0, -255 / 2],
-        [255 / 2, 255, 255 / 2]
-      );
-
-      const boxGroup = new THREE.Group();
-      boxGroup.add(
-        this.createLine(
-          "80,80,80",
-          [255 / 2, 0, -255 / 2],
-          [255 / 2, 255, -255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [255 / 2, 0, -255 / 2],
-          [255 / 2, 0, 255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [255 / 2, 0, 255 / 2],
-          [255 / 2, 255, 255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [-255 / 2, 0, 255 / 2],
-          [255 / 2, 0, 255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [-255 / 2, 255, -255 / 2],
-          [255 / 2, 255, -255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [-255 / 2, 255, -255 / 2],
-          [-255 / 2, 255, 255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [-255 / 2, 0, 255 / 2],
-          [-255 / 2, 255, 255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [255 / 2, 255, -255 / 2],
-          [255 / 2, 255, 255 / 2]
-        ),
-        this.createLine(
-          "80,80,80",
-          [255 / 2, 255, 255 / 2],
-          [-255 / 2, 255, 255 / 2]
-        )
-      );
-
-      data.linesGroup = new THREE.Group();
-      data.linesGroup.add(
-        red,
-        blue,
-        green,
-        cyan,
-        magenta,
-        yellow,
-        white,
-        boxGroup
-      );
-
-      data.gridHelper = new THREE.GridHelper(255, 25);
-    }
-
-    createCubes() {
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const distanceBetweenCubes = 1;
-      const maxValue = 255;
-
-      for (const value of data.colors) {
-        const x = value.red;
-        const y = value.green;
-        const z = value.blue;
-        const material = new THREE.MeshBasicMaterial({
-          color: value.hex,
-        });
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.x =
-          x * distanceBetweenCubes - maxValue * distanceBetweenCubes * 0.5;
-        mesh.position.y = y * distanceBetweenCubes;
-        mesh.position.z =
-          z * distanceBetweenCubes - maxValue * distanceBetweenCubes * 0.5;
-        mesh.name = value.spinalCase;
-        data.scene.add(mesh);
-      }
+      this.resizeWindow = this.resizeWindow.bind(this);
     }
 
     connectedCallback() {
-      data.camera = new THREE.PerspectiveCamera(
+      const d = this.#data;
+
+      d.winWidth = window.innerWidth;
+      d.winHeight = window.innerHeight;
+
+      d.camera = new THREE.PerspectiveCamera(
         70,
-        data.winWidth / data.winHeight,
+        d.winWidth / d.winHeight,
         0.01,
         1000
       );
-      data.camera.position.z = 150;
+      d.camera.position.z = 150;
 
-      data.scene = new THREE.Scene();
-      data.scene.rotation.x = 100 / 1000;
-      data.scene.position.y = -125;
+      d.scene = new THREE.Scene();
+      d.scene.rotation.x = 0.1;
+      d.scene.position.y = -125;
 
       this.createGuides();
       this.createCubes();
 
-      data.renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true,
-      });
+      d.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
       this.resizeWindow();
-      this.appendChild(data.renderer.domElement);
+      this.appendChild(d.renderer.domElement);
 
-      data.controls = new OrbitControls(data.camera, data.renderer.domElement);
-
-      data.controls.minPolarAngle = data.minPolarAngle;
-      data.controls.maxPolarAngle = data.maxPolarAngle;
-
-      // data.controls.minDistance = data.minDistance;
-      data.controls.maxDistance = data.maxDistance;
+      d.controls = new OrbitControls(d.camera, d.renderer.domElement);
+      d.controls.minPolarAngle = d.minPolarAngle;
+      d.controls.maxPolarAngle = d.maxPolarAngle;
+      // d.controls.minDistance = d.minDistance;
+      d.controls.maxDistance = d.maxDistance;
 
       const url = new URLSearchParams(location.search);
-      if (url.has("grid")) {
-        data.scene.add(data.gridHelper);
-      }
+      if (url.has("grid")) d.scene.add(d.gridHelper);
+      if (url.has("guides")) d.scene.add(d.linesGroup);
 
-      if (url.has("guides")) {
-        data.scene.add(data.linesGroup);
-      }
-
-      data.renderer.setAnimationLoop((time) => {
-        data.scene.rotation.y = time / 5000;
-        data.renderer.render(data.scene, data.camera);
+      d.renderer.setAnimationLoop((time) => {
+        d.scene.rotation.y = time / 5000;
+        d.renderer.render(d.scene, d.camera);
       });
+
       window.addEventListener("resize", this.resizeWindow);
     }
 
     resizeWindow() {
-      data.winWidth = window.innerWidth;
-      data.winHeight = window.innerHeight;
-      data.renderer.setSize(data.winWidth, data.winHeight);
-      data.camera.aspect = data.winWidth / data.winHeight;
-      data.camera.updateProjectionMatrix();
+      const d = this.#data;
+      d.winWidth = window.innerWidth;
+      d.winHeight = window.innerHeight;
+      d.renderer.setSize(d.winWidth, d.winHeight);
+      d.camera.aspect = d.winWidth / d.winHeight;
+      d.camera.updateProjectionMatrix();
+    }
+
+    createLine(color, origin, destination) {
+      const material = new THREE.LineBasicMaterial({ color: `rgb(${color})` });
+      const points = [
+        new THREE.Vector3(...origin),
+        new THREE.Vector3(...destination),
+      ];
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+      return new THREE.Line(geometry, material);
+    }
+
+    createGuides() {
+      const d = this.#data;
+      const lines = [
+        this.createLine("255,0,0", [-127.5, 0, -127.5], [127.5, 0, -127.5]),
+        this.createLine("0,0,255", [-127.5, 0, -127.5], [-127.5, 0, 127.5]),
+        this.createLine("0,255,0", [-127.5, 0, -127.5], [-127.5, 255, -127.5]),
+        this.createLine("0,255,255", [-127.5, 0, -127.5], [-127.5, 255, 127.5]),
+        this.createLine("255,0,255", [-127.5, 0, -127.5], [127.5, 0, 127.5]),
+        this.createLine("255,255,0", [-127.5, 0, -127.5], [127.5, 255, -127.5]),
+        this.createLine(
+          "255,255,255",
+          [-127.5, 0, -127.5],
+          [127.5, 255, 127.5]
+        ),
+      ];
+
+      const boxGroup = new THREE.Group();
+      const grayLines = [
+        [
+          [127.5, 0, -127.5],
+          [127.5, 255, -127.5],
+        ],
+        [
+          [127.5, 0, -127.5],
+          [127.5, 0, 127.5],
+        ],
+        [
+          [127.5, 0, 127.5],
+          [127.5, 255, 127.5],
+        ],
+        [
+          [-127.5, 0, 127.5],
+          [127.5, 0, 127.5],
+        ],
+        [
+          [-127.5, 255, -127.5],
+          [127.5, 255, -127.5],
+        ],
+        [
+          [-127.5, 255, -127.5],
+          [-127.5, 255, 127.5],
+        ],
+        [
+          [-127.5, 0, 127.5],
+          [-127.5, 255, 127.5],
+        ],
+        [
+          [127.5, 255, -127.5],
+          [127.5, 255, 127.5],
+        ],
+        [
+          [127.5, 255, 127.5],
+          [-127.5, 255, 127.5],
+        ],
+      ];
+
+      grayLines.forEach(([start, end]) =>
+        boxGroup.add(this.createLine("80,80,80", start, end))
+      );
+
+      d.linesGroup = new THREE.Group();
+      d.linesGroup.add(...lines, boxGroup);
+
+      d.gridHelper = new THREE.GridHelper(255, 25);
+    }
+
+    createCubes() {
+      const d = this.#data;
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const maxValue = 255;
+      const distance = 1;
+
+      for (const c of d.colors) {
+        const mesh = new THREE.Mesh(
+          geometry,
+          new THREE.MeshBasicMaterial({ color: c.hex })
+        );
+        mesh.position.set(
+          c.red * distance - maxValue * 0.5 * distance,
+          c.green * distance,
+          c.blue * distance - maxValue * 0.5 * distance
+        );
+        mesh.name = c.spinalCase;
+        d.scene.add(mesh);
+      }
     }
   }
 );
